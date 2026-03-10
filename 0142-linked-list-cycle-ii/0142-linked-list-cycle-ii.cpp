fd9fa90ffb98698b5b2e0cd3ae155aac1d5f4663
Muskansingh909 <muskan66504@gmail.com>
2026-03-11 00:13:23 +0530

    Time: 11 ms (15.49%), Space: 13.5 MB (10.12%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -6,22 +6,31 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-class Solution {
-public:
-    ListNode *detectCycle(ListNode *head) {
-        ListNode* slow=head;
-        ListNode* fast=head;
-        while(fast!=nullptr && fast->next!=nullptr ){
-            fast=fast->next->next;
-            slow=slow->next;
-            if(fast == slow)break;
+ class Solution{
+    public:
+ListNode *detectCycle(ListNode *head) {
+    // 1. Create a "Notebook" (Set) to remember where we've been
+    unordered_set<ListNode*> notebook;
+    
+    // 2. Start at the beginning of the list
+    ListNode* current = head;
+
+    // 3. Keep walking until we hit a dead end (nullptr)
+    while (current != NULL) {
+        
+        // 4. CHECK: Is this room already in our notebook?
+        if (notebook.count(current)) {
+            return current; // Yes! We found the start of the loop.
         }
-        if(fast==nullptr || fast->next==nullptr ) return nullptr;
-        slow=head;
-        while(slow!=fast){
-            slow=slow->next;
-            fast=fast->next;
-        }
-        return slow;
+        
+        // 5. If not, write this room in our notebook
+        notebook.insert(current);
+        
+        // 6. Move to the next room
+        current = current->next;
     }
+
+    // 7. If the loop ends, there was no cycle at all
+    return NULL;
+}
 };
